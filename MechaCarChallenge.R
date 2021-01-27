df63c88917aa0f85cc36c80d3aa7b6283b3163ca
kevin-green58 <71112005+kevin-green58@users.noreply.github.com>
2021-01-26 23:52:14 -0800

    Update MechaCarChallenge.R

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -3,3 +3,13 @@ lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,
 summary1 = lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg)
 summary(summary1)
 
+install.packages("dplyr")
+Suspension_Coil <- read.csv("Suspension_Coil.csv", check.names = F, stringsAsFactors = F)
+total_summary <- Suspension_Coil %>% 
+  summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+lot_summary1 <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+t.test(Suspension_Coil$PSI, mu = 1500)
+t.test(Suspension_Coil$PSI, subset(Suspension_Coil, Manufacturing_Lot == "Lot1")$PSI, mu = 1500)
+t.test(Suspension_Coil$PSI, subset(Suspension_Coil, Manufacturing_Lot == "Lot2")$PSI, mu = 1500.2)
+t.test(Suspension_Coil$PSI, subset(Suspension_Coil, Manufacturing_Lot == "Lot3")$PSI, mu = 1496.14)
